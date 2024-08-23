3690f3a7232347f1eb2180f9adcc5db962f29556
amonroy-2023021 <amonroy-2023021@kinal.edu.gt>
2024-08-23 16:41:57 -0600

    feat: agregaron los atributos y metodos de la clase Cuadrado

--- Poo_Ejercicio_2/src/model/Cuadrado.java
+++ Poo_Ejercicio_2/src/model/Cuadrado.java
@@ -1,5 +1,17 @@
 package model;
 
-public class Cuadrado {
+public class Cuadrado extends Forma{
+    private double lado;
+
+    public Cuadrado(String color, double lado) {
+    super(color);
+    this.lado = lado;
+}
+
+   @Override
+public double calcularArea() {
+    return lado * lado; 
+}
+
 
 }
