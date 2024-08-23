5d7b5a59a6e7173c46cbb8217a427f4c67940bcb
amonroy-2023021 <amonroy-2023021@kinal.edu.gt>
2024-08-23 15:41:33 -0600

    build: Se genero el proyecto y se creo la clase Forma

---
+++ Poo_Ejercicio_2/src/model/Forma.java
@@ -0,0 +1,19 @@
+package model;
+
+public class Forma {
+
+    private String Color;
+
+
+    public void Forma(){
+    }
+
+    public Forma(String Color){
+    this.Color = Color;
+    }
+
+    
+
+    
+
+}
